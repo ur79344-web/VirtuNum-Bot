b25a5be5cae28b20548822cfb0a16756c665faeb
ur79344-web <ur79344@gmail.com>
2026-07-29 00:19:12 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
- 
+ import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+SALESEEN_API_KEY = os.getenv("SALESEEN_API_KEY")
+SALESEEN_API_URL = os.getenv("SALESEEN_API_URL")
+
+CHANNEL_1 = os.getenv("CHANNEL_1")
+CHANNEL_2 = os.getenv("CHANNEL_2")
