bdeb40ca9091e5cece2b44486af9d918ef037d1f
ur79344-web <ur79344@gmail.com>
2026-07-29 09:14:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 from config import BOT_TOKEN
 from handlers import router
@@ -10,12 +12,18 @@ from database import create_tables
 async def main():
     create_tables()
 
-    bot = Bot(token=BOT_TOKEN)
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(
+            parse_mode=ParseMode.HTML
+        )
+    )
+
     dp = Dispatcher()
 
     dp.include_router(router)
 
-    print("✅ Bot ishga tushdi!")
+    print("✅ VirtuNum Bot ishga tushdi!")
 
     await dp.start_polling(bot)
 
