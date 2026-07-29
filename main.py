b9869645e358238dd888e0a5fa73eae920007b32
ur79344-web <ur79344@gmail.com>
2026-07-29 09:11:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
- 
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+from handlers import router
+from database import create_tables
+
+
+async def main():
+    create_tables()
+
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(router)
+
+    print("✅ Bot ishga tushdi!")
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
